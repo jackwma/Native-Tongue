ab95f194d16f9ced6cc6bfab7701a99cf44b5e61
jackwma <jackwma@uw.edu>
2019-01-13 03:47:12 -0800

    node.js script added

--- main.py
+++ main.py
@@ -10,36 +10,3 @@ def hello():
 
 if __name__ == '__main__':
     app.run()
-
-
-"""""
-# Imports the Google Cloud client library
-from google.cloud import speech
-from google.cloud.speech import enums
-from google.cloud.speech import types
-
-# Instantiates a client
-client = speech.SpeechClient()
-
-# The name of the audio file to transcribe
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'resources',
-    'audio.raw')
-
-# Loads the audio into memory
-with io.open(file_name, 'rb') as audio_file:
-    content = audio_file.read()
-    audio = types.RecognitionAudio(content=content)
-
-config = types.RecognitionConfig(
-    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=16000,
-    language_code='en-US')
-
-# Detects speech in the audio file
-response = client.recognize(config, audio)
-
-for result in response.results:
-    print('Transcript: {}'.format(result.alternatives[0].transcript))
-"""""
